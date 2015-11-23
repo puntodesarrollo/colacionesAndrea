36f948367ae43a56e4616d824b97e7d6868a361f
lpastenpinto <lpastenpinto@gmail.com>
2015-11-23 17:28:51 -0300

    productos

---
+++ admin/productos/metodoEditar.php
@@ -0,0 +1,24 @@
+<?php
+	
+	include $_SERVER['DOCUMENT_ROOT']."/admin/conexion.php";
+	$id = $_POST["id"];
+	$nombre = $_POST["nombre"];
+	$cantidad = $_POST["cantidad"];
+	$descripcion = $_POST["descripcion"];
+	$precio = $_POST["precio"];
+	$mostrar = $_POST["mostrar"];
+
+		    
+	$resultado = $con->query("UPDATE productos set nombre='$nombre', descripcion='$descripcion', cantidad='$cantidad', precio='$precio', mostrar='$mostrar' WHERE id='$id'");
+	
+
+	$delete = $con->query("DELETE FROM dias_productos WHERE id_producto='$id'");  
+
+	$dias = $_POST["dias"];	
+    foreach ($dias as $dia) {  
+    	$insert = $con->query("INSERT INTO dias_productos (id_producto,dia) VALUES('$id','$dia')");    	   
+    	
+    }
+	
+	header("location:/admin/productos");
+?>
