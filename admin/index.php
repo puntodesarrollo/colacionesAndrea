02b6601f2fb2652b6893f1b68ee8aefdc8b419eb
gpuellestorres <gpuellestorres@gmail.com>
2016-02-17 10:49:51 -0300

    Corrección enlace admin - nueva noticia

--- admin/index.php
+++ admin/index.php
@@ -37,7 +37,7 @@
 		<div class="text-center">
 			<br>
 			<a href="/admin/noticias" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-list-alt"></span>&nbsp;Ver Todos</a>
-			<a href="/admin/productos/agregar.php" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-plus"></span>&nbsp;Agregar Nuevo</a>
+			<a href="/admin/noticias/agregar.php" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-plus"></span>&nbsp;Agregar Nuevo</a>
 		</div>
 	</div>
 	
